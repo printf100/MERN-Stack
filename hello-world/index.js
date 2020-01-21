36973b04cd17213bd132ae579db966b61fbbbb27
printf100 <mickey0502@naver.com>
2020-01-21 19:05:39 +0900

    20200121 Static 페이지 만들기

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,15 +1,14 @@
-// require(모듈_이름) : node.js에서 기본적으로 주어지는 함수, modules폴더 안에 설치된 모듈을 불러옴
-var express = require('express');   // 설치한 express module을 불러와서 변수(express)에 담기
-var app = express();    // express를 실행하여 app Object 초기화 (Express framework에서 항상 가장 처음하는 것)
+var express = require('express');
+var app = express();
 
-// 서버에 get요청이 있는 경우,
-app.get('/', function(req, res) {   // req : request에 관련된 값들과 함수들이 저장되어 있는 object. HTTP request header, 요청 url, cookies, query, body 등의 정보가 저장되어 있다.
-                                    // res : response에 관련된 값들과 함수들이 저장되어 있는 object. HTTP response header, cookies, HTTP code 등의 정보를 확인하고 값을 변경할 수도 있다.
-    res.send('Hello World!');
-});
+//  HTTP method나 route에 상관없이 서버에 요청이 올 때마다 무조건 콜백함수가 실행됨
+app.use(express.static(__dirname + '/public')); // __dirname은 node.js에서 프로그램이 실행중인 파일의 위치를 나타내는 global variable
+// '현재_위치/public' route를 static폴더로 지정하라는 명령어
+// 즉 '/'에 접속하면 '현재_위치/public'를, '/css'에 접속하면 '현재_위치/public/css'를 연결해줌
+// static route에 접근할 때, 특별히 파일 이름을 지정해 주지 않으면 자동으로 index.html 파일을 찾게 됩니다.
+// 즉 '/'에 접속하면 '현재_위치/pulbic/index.html' 파일을 연결하게 됩니다.
 
-var port = 3000;    // 사용할 포트 번호
-// 서버가 실행되는 경우,
-app.listen(port, function() {   // 포트에 node.js 서버 연결
+var port = 3000;
+app.listen(port, function() {
     console.log('SERVER ON! http://localhost:'+port);
 });
